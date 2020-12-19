aca9bb200b28540ef619637254c5d535c9417d75
xfrnk2 <xfrnk2@gmail.com>
2020-12-20 01:27:22 +0900

    usedState와 useEffect를 사용한 crud 구현 진행

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import axios from'axios';
 
-const Todo = (title, content) => {
+const Todo = (todo) => {
     return (
-    <div className='todo'>Title : {title}
-        Content : {content}
+        
+    <div>
+        Title : {todo.title}
+        Content : {todo.content}
     </div>
     )
 }
