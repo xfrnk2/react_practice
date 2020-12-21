6e8c6286b3042fa69b31e767777c3808a664e5d3
xfrnk2 <xfrnk2@gmail.com>
2020-12-22 00:55:10 +0900

    Todo 데이터 수정 기능 구현

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, Fragment} from 'react';
 import axios from 'axios';
 
 const Todo = ({todo, todos, setTodos}) => {
     const [editMode, setEditMode] = useState(false);
+    const [form, setForm] = useState({
+        title: todo.title,
+        content: todo.content
+    });
 
-    const update = (event) => {
-   
-
-    };
+    const changeForm= (data) => {
+        setForm({
+            ...form, [data.target.name]: data.target.value
+        })
+    }
 
     const del = (todos) => {
         axios.delete('/todos/'+ todo.id)
@@ -23,20 +28,48 @@ const Todo = ({todo, todos, setTodos}) => {
         })
     }
     
+    const update = (event) => {
+        event.preventDefault();
+
+        axios.patch('/todos/' + todo.id, form)
+        .then((response) => {
+            setTodos(todos.map((todoData) => {
+                if (todoData.id === todo.id) {
+                    return response.data
+                }
+                return todoData
+            }
+            ))
+            changeEditMode();
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+
+
     const changeEditMode = () => {
         setEditMode(!editMode)
     }
     
     return (
     <div>
-
-
+        {editMode?
+           <form onSubmit={(event) => {update(event)}}>
+            <input type="text" name="title" value={form.title} placeholder="할일 제목" onChange={(event) => changeForm(event)}></input>
+            <input type="text" name="content" value={form.content} placeholder="할일 내용" onChange={(event) => changeForm(event)}></input>
+            <button type="submit">수정완료</button>
+            <button type="button" onClick={() => {changeEditMode()}}>취소</button>
+           </form>
+        :
+        <div>
         Title : {todo.title}
         Content : {todo.content}
-        <button onClick={update}>수정</button>
+        <button onClick={() => {changeEditMode()}}>수정</button>
         <button onClick={() => {del(todos)}}>삭제</button>
+        </div>
+        }
     </div>
-   
     )
 }
 export default Todo
